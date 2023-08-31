de43c9badefd4ee9f566ad67fe7fe6b5d88810a2
Super User <root@localhost.localdomain>
2023-08-31 10:33:38 -0600

    moved keybinds to keymaps.lua

--- init.lua
+++ init.lua
@@ -1,14 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-vim.keymap.set("n", "ge", vim.diagnostic.open_float, { noremap = true })
-vim.keymap.set("n", "gR", vim.lsp.buf.rename, { noremap = true })
-vim.keymap.set("n", "ff", "<Cmd>Neotree focus<CR>", { noremap = true })
--- vim.keymap.set("n", "<C-d>", function()
--- 	if #vim.api.nvim_tabpage_list_wins(0) > 1 then
--- 		vim.api.nvim_win_close(0, false)
--- 	end
--- end, { noremap = false })
+require("config.keymaps")
 
 vim.cmd([[
 set nowrap
@@ -16,6 +8,7 @@ set number
 set norelativenumber
 set tabstop=4
 set shiftwidth=4
+set softtabstop=4
 set expandtab
 set mouse=a
 
@@ -23,14 +16,10 @@ set mouse=a
 set foldlevelstart=99
 set foldmethod=indent
 
-" bind group comment to Ctrl-/
-map <C-_> gc
-
 " enable ruler at 80 chars
 set colorcolumn=80
 hi ColorColumn guibg=#303030
 
-noremap <C-d> <Cmd>q<CR>
 ]])
 
 function P(arg)
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+local noremap = { noremap = true }
+local remap = { remap = true }
+
+require("noice") -- load noice now so the <C-f> bind doesnt get overwritten
+map("n", "<C-f>", "<Cmd>Neotree focus<CR>", noremap)
+
+-- some lsp binds
+map("n", "ge", vim.diagnostic.open_float, noremap)
+map("n", "gR", vim.lsp.buf.rename, noremap)
+
+map("n", "<C-_>", "V<C-_>", remap)
+map("v", "<C-_>", "gc", remap) -- group comment to ctrl-/
+map("n", "<C-d>", "<Cmd>q<CR>", noremap) -- :q to ctrl-d
