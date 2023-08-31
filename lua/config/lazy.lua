477ff4fa9a5453a8f47cc49b561faaf185a75f18
David Fresco <davidfthesun@gmail.com>
2023-08-30 18:21:47 -0600

    removed redundant python lsps :/

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,7 +10,7 @@ require("lazy").setup({
 	spec = {
 		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
-		{ import = "lazyvim.plugins.extras.lang.python" },
+		-- { import = "lazyvim.plugins.extras.lang.python" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		-- { import = "lazyvim.plugins.extras.ui.mini-animate" },
 		{ import = "plugins" },
