52ac64b4d96f43b5ac2075465706adaa77282408
Super User <root@localhost.localdomain>
2023-09-12 10:57:11 -0600

    updated ruler color

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("config.keymaps")
 
 vim.cmd([[
 set nowrap
@@ -18,7 +17,7 @@ set foldmethod=indent
 
 " enable ruler at 80 chars
 set colorcolumn=80
-hi ColorColumn guibg=#303030
+hi ColorColumn guibg=#2f302a
 
 ]])
 
