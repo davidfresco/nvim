7afa69415f2e0972aff2bc582636f75852517fa0
davidfresco <davids@email.com>
2023-09-22 08:30:47 -0600

    added install script for the rest of neovim

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if which dnf &>/dev/null; then
+	sudo dnf -y install ninja-build cmake gcc make unzip gettext curl git
+elif which apt-get &>/dev/null; then
+	sudo apt-get install ninja-build gettext cmake unzip curl git
+elif which zypper &>/dev/null; then
+	sudo zypper install ninja cmake gcc-c++ gettext-tools curl git
+elif which pacman &>/dev/null; then
+	sudo pacman -S base-devel cmake unzip ninja curl git
+else
+	echo "unsupported package manager lol" >&2
+	exit 1
+fi
+
+git clone https://github.com/neovim/neovim ~/neovim
+cd ~/neovim
+git checkout stable
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+make install
+nvim --headless "+Lazy! sync" +qa
