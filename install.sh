ea19774bdc11c9575f223cd044cdf111f1ad1c4d
root <root@BLR-LT-DFRESCO.jmawireless.local>
2023-09-22 10:54:19 -0600

    added lang servers and extra dependencies

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+extra_deps="git npm"
+language_servers=( "pyright" "jedi-language-server" )
+
 if which dnf &>/dev/null; then
-	sudo dnf -y install ninja-build cmake gcc make unzip gettext curl git
+	sudo dnf -y install ninja-build cmake gcc make unzip gettext curl python3-venv $extra_deps
 elif which apt-get &>/dev/null; then
-	sudo apt-get -y install ninja-build gettext cmake unzip curl git
+	sudo apt-get -y install ninja-build gettext cmake unzip curl python3-venv $extra_deps
 elif which zypper &>/dev/null; then
-	sudo zypper -y install ninja cmake gcc-c++ gettext-tools curl git
+	sudo zypper -y install ninja cmake gcc-c++ gettext-tools curl $extra_deps
 elif which pacman &>/dev/null; then
-	sudo pacman -Sy base-devel cmake unzip ninja curl git
+	sudo pacman -Sy base-devel cmake unzip ninja curl $extra_deps
 else
 	echo "unsupported package manager lol" >&2
 	exit 1
@@ -21,3 +24,6 @@ sudo make install
 mkdir ~/.config
 git clone https://github.com/davidfresco/nvim ~/.config/nvim
 nvim --headless "+Lazy! sync" +qa
+for lang_server in ${language_servers[@]}; do
+    nvim --headless "+MasonInstall $lang_server" +qa
+done
