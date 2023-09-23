a913415f0adc8c6903a12d66620f3b8786ef2fc8
davidfresco <davids@email.com>
2023-09-23 12:36:04 -0600

    updated install deps for fedora

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 extra_deps="git npm"
-language_servers=( "pyright" "jedi-language-server" )
+language_servers=("pyright" "jedi-language-server")
 
 if which dnf &>/dev/null; then
-	sudo dnf -y install ninja-build cmake gcc make unzip gettext curl python3-venv $extra_deps
+	sudo dnf -y install ninja-build cmake gcc make unzip gettext curl $extra_deps
 elif which apt-get &>/dev/null; then
 	sudo apt-get -y install ninja-build gettext cmake unzip curl python3-venv $extra_deps
 elif which zypper &>/dev/null; then
@@ -25,5 +25,5 @@ mkdir ~/.config
 git clone https://github.com/davidfresco/nvim ~/.config/nvim
 nvim --headless "+Lazy! sync" +qa
 for lang_server in ${language_servers[@]}; do
-    nvim --headless "+MasonInstall $lang_server" +qa
+	nvim --headless "+MasonInstall $lang_server" +qa
 done
