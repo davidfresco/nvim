e14a525b7025445b612aa86aa6584dce07e5da46
root <root@LAPTOP-KL180AE5>
2023-11-02 15:13:05 -0600

    commented out mandatory lsp imports

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,24 +1,24 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"williamboman/mason.nvim",
-		},
-		opts = {
-			diagnostics = {
-				virtual_text = false,
-			},
-			servers = {
-				pyright = {
-					settings = {
-						pyright = {
-							disableLanguageServices = true,
-						},
-					},
-				},
-				jedi_language_server = {},
-				yamlls = {},
-			},
-		},
-	},
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+        },
+        opts = {
+            diagnostics = {
+                virtual_text = false,
+            },
+            servers = {
+                -- pyright = {
+                -- 	settings = {
+                -- 		pyright = {
+                -- 			disableLanguageServices = true,
+                -- 		},
+                -- 	},
+                -- },
+                -- jedi_language_server = {},
+                -- yamlls = {},
+            },
+        },
+    },
 }
