c18fc589cccbf0cf15fe73ebea6a612e20c3885d
root <root@LAPTOP-KL180AE5>
2023-11-06 13:45:17 -0700

    enabled symbols outline

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,29 +1,31 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  -- bootstrap lazy.nvim
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+    -- bootstrap lazy.nvim
+    -- stylua: ignore
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+        lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
-	spec = {
-		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
-		-- { import = "lazyvim.plugins.extras.lang.typescript" },
-		-- { import = "lazyvim.plugins.extras.lang.json" },
-		{ import = "plugins" },
-	},
-	defaults = {
-		lazy = false,
-		version = false, -- always use the latest git commit
-	},
-	checker = { enabled = false }, -- automatically check for plugin updates
-	performance = {
-		rtp = {
-			disabled_plugins = {
-				"tohtml",
-				"tutor",
-			},
-		},
-	},
+    spec = {
+        { "LazyVim/LazyVim",                                       import = "lazyvim.plugins" },
+        { import = "lazyvim.plugins.extras.editor.symbols-outline" },
+        -- { import = "lazyvim.plugins.extras.lang.typescript" },
+        -- { import = "lazyvim.plugins.extras.lang.json" },
+        { import = "plugins" },
+    },
+    defaults = {
+        lazy = false,
+        version = false,        -- always use the latest git commit
+    },
+    checker = { enabled = false }, -- automatically check for plugin updates
+    performance = {
+        rtp = {
+            disabled_plugins = {
+                "tohtml",
+                "tutor",
+            },
+        },
+    },
 })
